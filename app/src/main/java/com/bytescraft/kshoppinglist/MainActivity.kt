f7da07b44d5ff3ea9120e2dc288ad0f6fa12b1b3
Karan Jatav <karanjatav95@gmail.com>
2021-01-12 17:42:45 +0530

    Initial commit

---
+++ app/src/main/java/com/bytescraft/kshoppinglist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bytescraft.kshoppinglist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
